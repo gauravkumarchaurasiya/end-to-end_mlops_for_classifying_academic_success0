5a6510aab4eda35fee9c32ee9d72f1e8ecd243e8
Gaurav Kumar Chaurasiya <2002gauravkumarchaurasiya@gmail.com>
2024-06-22 10:56:42 +0530

    File initial Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project demonstrates the implementation of a robust MLOps pipeline to classify academic success using machine learning techniques. By leveraging an academic success dataset, we explore data preprocessing, model training, hyperparameter tuning, and deployment within a streamlined MLOps framework. The project showcases end-to-end automation, ensuring efficient model development, continuous integration, continuous deployment, and monitoring to maintain high model performance in predicting student success outcomes.',
+    author='Gaurav Kumar Chaurasiya',
+    license='',
+)
